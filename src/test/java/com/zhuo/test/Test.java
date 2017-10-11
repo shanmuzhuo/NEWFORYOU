4f9c9ad1ff1ac892b5f5fc50f3bd21506e3a428f
shanmuzhuo <shanmuzhuo@foxmail.com>
2017-10-11 21:52:56 +0800

    添加视频的处理包javacv

--- src/test/java/com/zhuo/test/Test.java
+++ src/test/java/com/zhuo/test/Test.java
@@ -1,11 +1,15 @@
 package com.zhuo.test;
 import java.awt.Image; 
-import java.awt.image.BufferedImage; 
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
 import java.io.File; 
-import javax.imageio.ImageIO; 
+import javax.imageio.ImageIO;
+
+import org.bytedeco.javacpp.BytePointer;
 import org.bytedeco.javacpp.opencv_core.IplImage; 
 import org.bytedeco.javacv.FFmpegFrameGrabber; 
-import org.bytedeco.javacv.Frame; 
+import org.bytedeco.javacv.Frame;
 public class Test {
 	/**
 	 * 获取指定视频的帧并保存为图片至指定目录
@@ -18,6 +22,7 @@ public class Test {
 	    long start = System.currentTimeMillis();
 	    File targetFile = new File(framefile);
 	    FFmpegFrameGrabber ff = new FFmpegFrameGrabber(videofile); 
+//	    ff.start();
 	    ff.start();
 	    int lenght = ff.getLengthInFrames();
 	    int i = 0;
@@ -31,18 +36,36 @@ public class Test {
 	        i++;
 	    }
 	    IplImage img = f.image;
+	    //OpenCVFrameConverter.ToIplImage converter = new OpenCVFrameConverter.ToIplImage();
+	   // IplImage img = converter.convertToIplImage(f);
 	    int owidth = img.width();
 	    int oheight = img.height();
 	    // 对截取的帧进行等比例缩放
 	    int width = 800;
 	    int height = (int) (((double) width / owidth) * oheight);
 	    BufferedImage bi = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
-	    bi.getGraphics().drawImage(f.image.getBufferedImage().getScaledInstance(width, height, Image.SCALE_SMOOTH),
+	    bi.getGraphics().drawImage(iplToBufImgData(img).getScaledInstance(width, height, Image.SCALE_SMOOTH),
 	            0, 0, null);
+/*	    bi.getGraphics().drawImage(f.image.getBufferedImage().getScaledInstance(width, height, Image.SCALE_SMOOTH),
+	            0, 0, null);*/
 	    ImageIO.write(bi, "jpg", targetFile);
 	    ff.stop();
 	    System.out.println(System.currentTimeMillis() - start);
 	}
+
+	public static BufferedImage iplToBufImgData(IplImage mat) {  
+	    if (mat.height() > 0 && mat.width() > 0) {  
+	        BufferedImage image = new BufferedImage(mat.width(), mat.height(),  
+	            BufferedImage.TYPE_3BYTE_BGR);  
+	        WritableRaster raster = image.getRaster();  
+	        DataBufferByte dataBuffer = (DataBufferByte) raster.getDataBuffer();  
+	        byte[] data = dataBuffer.getData();  
+	        BytePointer bytePointer =new BytePointer(data);  
+	       mat.imageData(bytePointer);  
+	        return image;  
+	    }  
+	    return null;  
+	 }  
 
 	public static void main(String[] args) {
 	    try {
